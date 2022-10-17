153566e1795615b98159f5062a89c0f97b0121e3
kostya <kostya@kut192-168-0-039.camera.kochi-tech.ac.jp>
2022-10-17 14:38:00 +0900

    first commit

---
+++ settings.py
@@ -0,0 +1,48 @@
+from os import environ
+
+SESSION_CONFIGS = [
+    dict(
+        name='SRI_new',
+        app_sequence=['SRI_new'],
+        num_demo_participants=2,
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc="",
+    mturk_hit_settings=dict(
+        keywords='SRI, Sustainability, Capital',
+        title='Social Investment - Experimental Study',
+        description='Experiment',
+        frame_height=500,
+        template='global/mturk_template.html',
+        minutes_allotted_per_assignment=60,
+        expiration_hours=7 * 24,
+        qualification_requirements=[]
+        # grant_qualification_id='YOUR_QUALIFICATION_ID_HERE', # to prevent retakes
+        ),
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'JPY'
+USE_POINTS = True
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = '7937348957020'
