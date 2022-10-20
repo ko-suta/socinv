10ca21c82c15c7c2e30b27ae95a8fb5e1df6eac5
kostya <kostya@kut192-168-0-039.camera.kochi-tech.ac.jp>
2022-10-20 12:01:09 +0900

    import sentry

--- survey/__init__.py
+++ survey/__init__.py
@@ -6,7 +6,7 @@ This is a survey containing general questions.
 
 class Constants(BaseConstants):
     name_in_url = 'survey'
-    players_per_group = 1
+    players_per_group = None
     num_rounds = 1
 
 
